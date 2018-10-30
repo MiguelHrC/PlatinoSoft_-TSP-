95a06077c2b3a7e98d95885a6e4bdf75d281048e
Alejandra <ale.zamo_gutierrez@hotmail.com>
2018-10-30 11:36:36 -0600

    Se modificó clase Perfil.php a Usuarios.php

---
+++ 02. Desarrollo del proyecto/01.Primer ciclo/03.Componentes de sistema/01.Semana 3/01.Clases/Usuarios.php
@@ -0,0 +1,43 @@
+<?php
+	
+	class Perfil{
+
+		private $idUsuarios;
+		private $Nombre_Usuario;
+		private $Correo;
+		private $Contrasena;
+
+		function __construct(){
+			$this -> idUsuarios = 0;
+			$this -> Nombre_Usuario = null;
+			$this -> Correo_Electronico = null;
+			$this -> Contrasena = null;
+		}
+
+		public function setidUsuarios($idUsuarios){
+			$this -> idUsuarios = $idUsuarios;
+		}
+		public function getidUsuarios(){
+			return $this -> idUsuarios;
+		}
+		public function setNombre_Usuario($Nombre_Usuario){
+			$this -> Nombre_Usuario = $Nombre_Usuario;
+		}
+		public function getNombre_Usuario(){
+			return $this -> Nombre_Usuario;
+		}
+		public function setCorreo($Correo){
+			$this -> Correo = $Correo;
+		}
+		public function getCorreo(){
+			return $this -> Correo;
+		}
+		public function setContrasena($Contrasena){
+			$this -> Contrasena = $Contrasena;
+		}
+		public function getContrasena(){
+			return $this -> Contrasena;
+		}
+	}
+
+?>
