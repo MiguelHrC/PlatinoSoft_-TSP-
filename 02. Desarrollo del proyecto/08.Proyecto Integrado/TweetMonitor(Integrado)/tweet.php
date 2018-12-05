b6937ad6bba5c25f2d2ed86318093757e150d109
Batres35 <juanluis.batresjuarez@live.com>
2018-12-05 11:26:09 -0600

    Actualizacion de programa
    Ahora se puede agregar varios permisos en un solo tweet

--- 02. Desarrollo del proyecto/08.Proyecto Integrado/TweetMonitor(Integrado)/tweet.php
+++ 02. Desarrollo del proyecto/08.Proyecto Integrado/TweetMonitor(Integrado)/tweet.php
@@ -12,25 +12,31 @@
 		$usuario = $_POST['usuario'];
 		$texto = $_POST['texto'];
 		$hashtag = BuscarHashtag($texto);
-		$permiso = BuscarPermiso($texto);
+		$permisos = BuscarPermiso($texto);
 
 		if (strcasecmp($hashtag,"permiso") == 0){
-			$query = "INSERT INTO permisos 
-			(usuario_twitter, usuario_tweetmonitor) 
-			VALUES 
-			('$usuario', '$permiso')";
+			foreach ($permisos as $permiso) {
+				$query = "INSERT INTO permisos 
+				(usuario_twitter, usuario_tweetmonitor) 
+				VALUES 
+				('$usuario', '$permiso')";
+				if ($conexion->query($query) === TRUE) {
+					echo "Permiso agregado exitosamente!\n<br>";
+				} else {
+					echo "Error en la consulta: ".$query."\n Error: ".$conexion->error; 
+				}
+			}
 		}
 		else{
 			$query = "INSERT INTO tweets 
 			(usuario, fecha, texto, hashtag) 
 			VALUES 
 			('$usuario', '$fecha', '$texto', '$hashtag')";
-		}
-
-		if ($conexion->query($query) === TRUE) {
-			echo "Agregado exitosamente!";
-		} else {
-			echo "Error en la consulta: ".$query."\n Error: ".$conexion->error; 
+			if ($conexion->query($query) === TRUE) {
+				echo "Tweet Agregado exitosamente!";
+			} else {
+				echo "Error en la consulta: ".$query."\n Error: ".$conexion->error; 
+			}
 		}
 	}
 
@@ -57,18 +63,37 @@
 	}
 
 	function BuscarPermiso($texto){
-		$permiso = "";
-		if (stripos($texto,"$") > -1){
-			$sign = stripos($texto,"$");
-			if (stripos($texto," ",$sign) != false){
-				$space = stripos($texto," ",$sign);
-				$permiso = substr($texto, $sign+1, $space-$sign-1);
+		$inicio = 0;
+		$contador = 0;
+		$permiso = array();
+		$ultimo = false;
+		
+		while (!$ultimo) { 
+			$subcadena = substr($texto, $inicio, strlen($texto));
+			if (stripos($subcadena,"$") > -1){
+				$sign = stripos($subcadena,"$");
+				if (stripos($subcadena," ",$sign) != false){
+					$space = stripos($subcadena," ",$sign);
+					if (substr($subcadena, $sign+1, $space-$sign-1) != ''){
+						$permiso[$contador] = substr($subcadena, $sign+1, $space-$sign-1);
+					}
+					$contador++;
+					$inicio += $space+1;
+				}
+				else
+				{
+					if (substr($subcadena, $sign+1, strlen($subcadena)-$sign-1)){
+						$permiso[$contador] = substr($subcadena, $sign+1, strlen($subcadena)-$sign-1);
+					}
+					$ultimo = true;
+				}
 			}
 			else
 			{
-				$permiso = substr($texto, $sign+1, strlen($texto)-$sign-1);
+				$ultimo = true;
 			}
 		}
+
 		return $permiso;
 	}
 
